274973ea05a12fac95b62705e4abee2d56bb303f
Jakob Martini <jakob.martini@stud.tu-darmstadt.de>
2026-05-30 19:31:11 +0200

    update params

--- utils.py
+++ utils.py
@@ -49,8 +49,8 @@ def seed_everything(seed=None) -> int:
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed_value)
 
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
 
     return seed_value
 
