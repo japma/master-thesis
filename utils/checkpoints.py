25ec738097ffa22cd876c3eb78083b0b679c94f3
Jakob Martini <jakob.martini@stud.tu-darmstadt.de>
2026-06-10 16:47:43 +0200

    wip: cspn config

--- utils/checkpoints.py
+++ utils/checkpoints.py
@@ -2,8 +2,10 @@ from pathlib import Path
 import torch
 from models.autoencoder import AbstractAutoencoder, VariationalAutoencoder
 from models.cspn import AbstractCSPN
+from models.cspn.einet import Einet
 
 
+# --- Autoencoder ---
 def save_autoencoder(model: AbstractAutoencoder, path: Path) -> None:
     path.parent.mkdir(parents=True, exist_ok=True)
     torch.save(
@@ -37,6 +39,7 @@ def load_autoencoder(path: Path, device=None) -> AbstractAutoencoder:
     return model
 
 
+# --- CSPN ---
 def save_cspn(model: AbstractCSPN, path: Path) -> None:
     path.parent.mkdir(parents=True, exist_ok=True)
     torch.save(
@@ -46,11 +49,22 @@ def save_cspn(model: AbstractCSPN, path: Path) -> None:
         },
         path,
     )
+    print("Saved CSPN checkpoint to", path)
+
+
+def _create_cspn_from_checkpoint(cfg: dict) -> AbstractCSPN:
+    return Einet(
+        num_vars=cfg["num_vars"],
+        context_dim=cfg["context_dim"],
+        num_leaves=cfg["num_leaves"],
+        num_nodes=cfg["num_nodes"],
+        nn_hidden_dim=cfg["nn_hidden_dim"],
+        nn_num_hidden_layers=cfg["nn_num_hidden_layers"],
+    )
 
 
 def load_cspn(path: Path, device=None) -> AbstractCSPN:
     ckpt = torch.load(path, map_location=device, weights_only=True)
-    # model = create_cspn(**ckpt["model_cfg"])
-    # model.load_state_dict(ckpt["model_state"])
-    # return model
-    pass
+    model = _create_cspn_from_checkpoint(ckpt["model_cfg"])
+    model.load_state_dict(ckpt["model_state"])
+    return model
