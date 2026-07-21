da32dae0982963ebe4070254ba39136ad37c39bb
Jakob Martini <jakob.martini@stud.tu-darmstadt.de>
2026-07-21 15:04:17 +0200

    fix ae configs

--- train_autoencoders.sh
+++ train_autoencoders.sh
@@ -8,9 +8,10 @@ source .venv/bin/activate
 
 configs=(
     "configs/autoencoder/mnist.yaml"
-    "configs/autoencoder/celeba.yaml"
-    "configs/autoencoder/flowers.yaml"
-    "configs/autoencoder/cub.yaml"
+    "configs/autoencoder/mnist_beta8.yaml"
+#    "configs/autoencoder/celeba.yaml"
+#    "configs/autoencoder/flowers.yaml"
+#    "configs/autoencoder/cub.yaml"
 #    "configs/autoencoder/mnist_beta4.yaml"
 #    "configs/autoencoder/mnist_beta8.yaml"
 #    "configs/autoencoder/flowers_beta4.yaml"
